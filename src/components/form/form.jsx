86c3f17d1a4efb39d25e87bc57bfa55f8cf8dd6f
Alexander Cheshire <a.g.cheshire@gmail.com>
2018-07-15 21:38:11 +0100

    Added linting to the project

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class Form extends Component {
   render() {
@@ -8,6 +8,6 @@ class Form extends Component {
       </div>
     );
   }
-};
+}
 
 export { Form };
